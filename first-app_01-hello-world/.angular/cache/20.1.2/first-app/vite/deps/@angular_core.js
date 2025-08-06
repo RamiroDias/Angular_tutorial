e0d946adbd688a2f9f8000528c41653968dc7c6b
RamiroDias <ramiro_v_dias@hotmail.com>
2025-08-06 21:22:53 +0100

    Completed tutorial

--- first-app_01-hello-world/.angular/cache/20.1.2/first-app/vite/deps/@angular_core.js
+++ first-app_01-hello-world/.angular/cache/20.1.2/first-app/vite/deps/@angular_core.js
@@ -485,7 +485,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-IOK7PU67.js";
+} from "./chunk-R6HBJOM5.js";
 import "./chunk-WDMUDEB6.js";
 export {
   ANIMATION_MODULE_TYPE,
--- first-app_01-hello-world/src/app/app.ts
+++ first-app_01-hello-world/src/app/app.ts
@@ -1,13 +1,24 @@
 import {Component} from '@angular/core';
+import {Home} from './home/home';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Home, RouterModule],
   template: `
-    <h1>Default</h1>
+    <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+    </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.css'],
 })
 export class App {
-  title = 'default';
+  title = 'homes';
 }
